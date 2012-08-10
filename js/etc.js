6002c313b66a730cff98042ed0635a345244a315
Rakhat Jabagin <neilhem@hotmail.com>
2012-08-09 18:03:48 -0700

    added etc.js stuff which u don't need

---
+++ js/etc.js
@@ -0,0 +1,18 @@
+$(document).ready( function(){	
+	$('.main_menu ul li').hover(
+        function() {
+            $(this).addClass('open');
+            $(this).find('ul').stop(true, true);
+            $(this).find('> ul').slideDown();
+        },
+        function() {
+            $(this).removeClass('open');
+            $(this).find('> ul').slideUp('fast'); 
+        }
+    );
+    if (window.PIE) {
+        $('.pie').each(function() {
+            PIE.attach(this);
+        });
+    };    
+});
