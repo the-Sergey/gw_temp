88dcdb4fde8b960af45bd1805e00468c0c6d68c0
neilhem <neilhem@hotmail.com>
2012-11-02 17:46:46 +0600

    Added Modernizr

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    
+    Modernizr.load({
+    	test: Modernizr.inputtypes.date,
+  		nope: ['http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js',
+  				'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js',
+  				'jquery-ui.css'],
+  		complete: function () {
+    		$('input[type=date]').datepicker({
+      		dateFormat: 'yy-mm-dd'
+   			});
+  		}
+	});
 });
