bd1c9c2aa776ddeb6d8585072ed22562ae770f11
Rakhat Jabagin <neilhem@hotmail.com>
2012-11-04 14:58:25 +0600

    Added input require polifills

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
+    if (Modernizr.inputtypes.date) {
+        return true;
+    } else {
+        $('input[type=date]').datepicker({
+            dateFormat: 'dd-mm-yy'
+        });        
+    }   
     Modernizr.load({
-    	test: Modernizr.inputtypes.date,
-  		nope: ['http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js',
-  				'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js',
-  				'jquery-ui.css'],
-  		complete: function () {
-    		$('input[type=date]').datepicker({
-      		dateFormat: 'yy-mm-dd'
-   			});
-  		}
-	});
+        test: Modernizr.input.required,
+        nope: 'js/libs/jquery.placeholder.min.js',
+        complete: function () {
+            $('input, textarea').placeholder();
+        }
+    });
+    // if(!Modernizr.input.placeholder){
+    //     $('[placeholder]').focus(function() {
+    //         var input = $(this);
+    //         if (input.val() == input.attr('placeholder')) {
+    //             input.val('');
+    //             input.removeClass('placeholder');
+    //         }
+    //     }).blur(function() {
+    //         var input = $(this);
+    //         if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    //         input.addClass('placeholder');
+    //         input.val(input.attr('placeholder'));
+    //     }
+    //     }).blur();
+    //     $('[placeholder]').parents('form').submit(function() {
+    //         $(this).find('[placeholder]').each(function() {
+    //             var input = $(this);
+    //             if (input.val() == input.attr('placeholder')) {
+    //             input.val('');
+    //             }
+    //         })
+    //     });
+    // }
 });
